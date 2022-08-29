b14b0f02706c82d1a3cf0ac03dca7fb054ab026d
monasky <mimijojosevo@yahoo.com>
2022-08-28 22:18:23 -0400

    added the package in loop.java

--- src/first/Loops.java
+++ src/first/Loops.java
@@ -1,6 +1,9 @@
-public class Loop {
- public seatic void main(String [] args) {
-   for(int i = 0; i < 5; i==) {
+package first;
+public class Loops {
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++) {
+    }
+
    }
  }
-}
+
